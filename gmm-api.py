920147ce0f99e70c86b66427ffe316a02938b1f8
brian <brian@bserv.(none)>
2014-04-24 12:55:16 -0700

    first write of script

--- gmm-api.py
+++ gmm-api.py
@@ -1,1 +1,26 @@
+#!/usr/bin/python
 import os
+import sys
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+class MyHandler(FileSystemEventHandler,place):
+	def on_created(self,event):
+		exec_string = "gmsync.py -m " + place + " " 
+		os.system(exec_string)
+
+
+if __name__ == "__main__":
+    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    event_handler = MyHandler(path)
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
